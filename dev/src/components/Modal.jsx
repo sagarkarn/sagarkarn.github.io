47753cb006b386423fa41b292371c98db2132d73
pratim roy <pratim@capitalnumbers.com>
2023-03-19 16:03:17 +0530

    browser list update  modal change

--- dev/src/components/Modal.jsx
+++ dev/src/components/Modal.jsx
@@ -5,9 +5,13 @@ function Modal({title, body, action}) {
     <div className="modal" id="my-modal">
       <div className="modal-box">
         <h3 className="font-bold text-lg">{title}</h3>
-        {body}
+        <p className="py-4">
+          <div className='text-center'>
+          <a href="http://linkedin.com/in/sagarkarn" target="_blank" rel="noreferrer" className='btn btn-primary'>click here</a>
+          </div>
+        </p>
         <div className="modal-action">
-        <a href="#" className="btn">Close</a>
+        <a href="#" className="btn btn-ghost" rel='noreferrer'>Close</a>
         </div>
       </div>
     </div>
